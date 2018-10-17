4fc6ee11d8472985370dbaa7bce72be8a43849b7
ritchie46 <ritchie46@gmail.com>
2018-10-17 11:39:10 +0200

    merge working btj

--- serverless/batch-transform/handler.py
+++ serverless/batch-transform/handler.py
@@ -1,8 +1,10 @@
 import json
+import time
+import random
 import os
+import re
 from datetime import date, datetime
 import boto3
-
 
 # SETTINGS
 DESIRED_COUNT = int(os.environ['DESIRED_COUNT'])
@@ -13,6 +15,7 @@ ECS_CLUSTER = os.environ['RESOURCE_NAME']
 TASK_DEFINITION = os.environ['RESOURCE_NAME']
 SUBNET = os.environ['SUBNET']
 SECURITY_GROUP = os.environ['SECURITY_GROUP']
+MODEL = os.environ['MODEL']
 
 s3 = boto3.resource('s3')
 sqs = boto3.resource('sqs')
@@ -28,22 +31,49 @@ def json_serial(obj):
 
 
 def lambda_handler(event, context):
+    try:
+        # determine the version of the latest data partitions.
+        q = re.compile(r'version=(\d*)')
+        version = max(map(lambda x: 0 if x is None else int(x.group(1)),
+                          map(lambda x: q.search(x['Key']),
+                              boto3.client('s3').list_objects_v2(Bucket=OUTPUT_BUCKET,
+                                                                 Prefix=os.path.join(OUTPUT_KEY, f'model={MODEL}'))[
+                                  'Contents']))) + 1
+    except KeyError:
+        version = 0
 
+    # Add messages to SQS Queue.
+    # The message contains:
+    # {   bucket: <input-data-bucket>,
+    #     key: <input-data-key>,
+    #     output_bucket: <storage-bucket>,
+    #     output_key: <storage-key>
+    #     model: <model-name>
+    #     version: <int>
+    # }
     event_info = event['Records'][0]['s3']
-
+    key = event_info['object']['key']
     q = sqs.get_queue_by_name(QueueName=SQS_NAME)
     message = json.dumps(dict(
         bucket=event_info['bucket']['name'],
-        key=event_info['object']['key'],
+        key=key,
         output_bucket=OUTPUT_BUCKET,
-        output_key=OUTPUT_KEY))
+        output_key=OUTPUT_KEY,
+        model=MODEL,
+        version=version))
 
     print(f'Add {message} to queue')
     response = q.send_message(
         MessageBody=message
     )
 
-    if len(ecs_client.list_services(cluster=ECS_CLUSTER)['serviceArns']) == 0:
+    # Add the key as seed so that all lambda sleep different times.
+    # Otherwise N files lead to N tasks
+    # The first sleep is so that the tasks don't start before the lambda's are finished. This will result in different
+    # versions.
+    time.sleep(60 + 120 * random.Random(key).random())
+    # if needed start the container in ECS
+    if len(ecs_client.list_tasks(cluster=ECS_CLUSTER)['taskArns']) == 0:
 
         print('RUN ECS task')
 
--- src/model/transform.py
+++ src/model/transform.py
@@ -2,13 +2,11 @@ import json
 import os
 import io
 import time
-from datetime import datetime
 import re
 import boto3
 import pandas as pd
 from wsgi import app
 from model import modelwrapper
-from model.data import prepare_data
 from cloudhelper import open_s3_file, write_s3_string
 
 sqs = boto3.resource('sqs', region_name=app.config['AWS_REGION'])
@@ -23,33 +21,53 @@ class BatchTransformJob:
         )
         self.messages = None
 
-    def fetch_messages(self):
-        self.messages = self.q.receive_messages()
+    def fetch_messages(self, n=10):
+        """
+        :param n: (int) Max number of messages is 10.
+        """
+        self.messages = self.q.receive_messages(MaxNumberOfMessages=n)
         return self.messages
 
     def process_q(self):
-        for message in self.messages:
-            m = json.loads(message.body)
+        version = None
+        while True:
+            for message in self.messages:
+                m = json.loads(message.body)
 
-            # The `=` sign is not properly encoded.
-            key = re.sub(r'version%\d*D', 'version=', m['key'], 1)
-            print(f"Downloading key: {key} from bucket: {m['bucket']}")
+                # make sure that the version doesn't change in case the lambda's weren't finished.
+                if version is None:
+                    version = m['version']
 
-            f = open_s3_file(m['bucket'], key)
-            df = pd.read_parquet(f)
+                # The `=` sign is not properly encoded.
+                key = re.sub(r'version%\d*D', 'version=', m['key'], 1)
+                print(f"Downloading key: {key} from bucket: {m['bucket']}")
 
-            print('Invoked with {} records'.format(df.shape[0]))
-            # Do the prediction
-            predictions = modelwrapper.predict(df)
+                f = open_s3_file(m['bucket'], key)
+                df = pd.read_parquet(f)
 
-            f = io.StringIO()
-            predictions.to_csv(f, index=False)
-            if write_s3_string(bucket=m['output_bucket'],
-                               key=os.path.join(f"{m['output_key']}",
-                                                datetime.now().strftime('%d-%m-%Y'), f"{int(time.time())}.csv"),
-                               f=f):
-                print('Success, delete message.')
-                message.delete()
+                print('Invoked with {} records'.format(df.shape[0]))
+                # Do the prediction
+                predictions = modelwrapper.predict(df)
+
+                try:
+                    fn = re.compile(r'[\\/]').split(key)[-1].split('.')[-2]
+
+                except IndexError:
+                    fn = time.time()
+                f = io.StringIO()
+                predictions.to_csv(f, index=False)
+                if write_s3_string(bucket=m['output_bucket'],
+                                   key=os.path.join(f"{m['output_key']}",
+                                                    f"model={m['model']}",
+                                                    f"version={version}",
+                                                    f"{fn}.csv"),
+                                   f=f):
+                    print('Success, delete message.')
+                    message.delete()
+
+            # break loop once all the messages are processed
+            if len(self.fetch_messages()) == 0:
+                return True
 
 
 def run_batch_transform_job():
@@ -73,7 +91,3 @@ def run_batch_transform_job():
 
     print('Found messages, process the queue')
     btj.process_q()
-
-
-
-
